dff64700372bc8c44cad607c80a8db8b025e21b2
Bitha17 <16521076@mahasiswa.itb.ac.id>
2023-05-03 18:33:50 +0700

    modify queries

--- backend/database/database.js
+++ backend/database/database.js
@@ -8,74 +8,102 @@ const connection = mysql.createConnection({
   database: 'gptkw'
 });
 
-async function addQuestion(question, answer) {
-    const existingQuestion = await connection.query("SELECT * FROM questions WHERE question like ?", [question]);
-  
-    if (existingQuestion.length > 0) {
-      // Jika pertanyaan sudah ada, maka hanya jawaban yang akan diperbarui
-      await connection.query("UPDATE questions SET answer = ? WHERE question = ?", [answer, question]);
-      return "Jawaban untuk pertanyaan ini telah diperbarui.";
-    } else {
-      // Jika pertanyaan belum ada, maka ditambahkan ke database
-      await connection.query("INSERT INTO questions (question, answer) VALUES (?, ?)", [question, answer]);
-      return "Pertanyaan baru berhasil ditambahkan.";
-    }
-  }
-  
+// Function to add a new question and answer to the database
+function addQuestion(question, answer) {
+  return new Promise((resolve, reject) => {
+    // Check if the question already exists in the database
+    connection.query(
+      'SELECT * FROM questions WHERE question = ?',
+      [question],
+      (error, results) => {
+        if (error) reject(error);
 
-function deleteQuestion(question) {
-    const query = `DELETE FROM questions WHERE question LIKE '%${question}%'`;
-  
-    connection.query(query, (err, result) => {
-      if (err) throw err;
-      console.log(`${result.affectedRows} question(s) deleted from database!`);
-    });
+        // If the question exists, update its answer
+        if (results.length > 0) {
+          connection.query(
+            'UPDATE questions SET answer = ? WHERE question = ?',
+            [answer, question],
+            (error, results) => {
+              if (error) reject(error);
+              console.log('Answer updated for question:', question);
+              resolve();
+            }
+          );
+        } // If the question doesn't exist, add a new question and answer
+        else {
+          connection.query(
+            'INSERT INTO questions (question, answer) VALUES (?, ?)',
+            [question, answer],
+            (error, results) => {
+              if (error) reject(error);
+              console.log('Question added to database:', question);
+              resolve();
+            }
+          );
+        }
+      }
+    );
+  });
 }
 
+// Function to delete a question from the database
+function deleteQuestion(question) {
+  return new Promise((resolve, reject) => {
+    connection.query("DELETE FROM questions WHERE question LIKE ?", [question], (err, result) => {
+      if (err) reject(err);
+      console.log(`${result.affectedRows} question(s) deleted from database!`);
+      resolve();
+    });
+  });
+}
+
+// Function to get the answer for a given question
 function getAnswer(question) {
-    // Koneksi ke database di sini
-    // Query untuk mencari jawaban berdasarkan pertanyaan
+  return new Promise((resolve, reject) => {
+    // Query to search for the answer based on the question
     const sql = "SELECT answer FROM questions WHERE question = ?";
-    
-    // Jalankan query dengan parameter pertanyaan
+
+    // Execute the query with the question parameter
     connection.query(sql, [question], function (error, results) {
-      if (error) throw error;
-      
-      // Jika jawaban ditemukan, kembalikan jawaban
+      if (error) reject(error);
+
+      // If an answer is found, return it
       if (results.length > 0) {
         const answer = results[0].answer;
-        return answer;
+        console.log(answer);
+        resolve(answer);
       } else {
-        // Jika tidak ada jawaban yang ditemukan, kembalikan pesan error
-        return "Maaf, saya tidak tahu jawabannya.";
+        // If no answer is found, return an error message
+        resolve("Maaf, saya tidak tahu jawabannya.");
       }
     });
-  }
-  
+  });
+}
 
-// example of use
+// Function to get all questions from the database
+function getAllQuestions() {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT question FROM questions', (error, results, fields) => {
+      if (error) reject(error);
+      const questions = results.map(row => row.question);
+      resolve(questions);
+    });
+  });
+}
 
-// function main() {
-//   const question = "When is the independence day of Indonesia?";
-//   const answer = "17 August 1945";
-  
-//   // Panggil fungsi addQuestion untuk menambahkan pertanyaan ke database
-//   addQuestion(question, answer)
-//     .then((result) => {
-//       console.log(result);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     })
-//     .finally(() => {
-//       // Menutup koneksi ke database setelah selesai
-//       connection.end();
-//     });
-
-//     getAnswer(question);
-
-//     deleteQuestion("What is the capital of France?");
+// Example of use
+// async function main() {
+//   const question = "What is the capital of France?";
+//   await deleteQuestion("When is the capital of France?");
+//   questions = await getAllQuestions();
+//   console.log(questions);
+//   await addQuestion("What is the capital of France?", "Paris")
+//   answer = await getAnswer(question);
+//   console.log("here:" + answer);
+//   questions = await getAllQuestions();
+//   console.log(questions);
+//   connection.end();
 // }
 
-// // Panggil fungsi main untuk memulai program
-// main();
+// Panggil fungsi main untuk memulai program
+main();
