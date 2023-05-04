4ce10fcfc327402eb89d3d9758b7543eda05493a
Bitha17 <16521076@mahasiswa.itb.ac.id>
2023-05-04 10:44:47 +0700

    add get_algo regex

--- backend/regex.js
+++ backend/regex.js
@@ -8,3 +8,17 @@ const date_regex = /\d{2}\/\d{2}\/\d{4}/;
 // add and delete question regex
 const add_question_regex = /^Tambah pertanyaan\s.+ dengan jawaban\s.+$/i;
 const delete_question_regex = /^Hapus pertanyaan/i;
+
+function get_algo(question) {
+    if (calculator_mode_regex.test(question)) {
+        return "Calculator";
+    } else if (date_regex.test(question)) {
+        return "Date";
+    } else if (add_question_regex.test(question)) {
+        return "Add Question";
+    } else if (delete_question_regex.test(question)) {
+        return "Delete Question";
+    } else {
+        return "Search";
+    }
+}
